8ef3b362ca5aff70ce0eb7dc202472a7da9ac865
Maurice Heymann <info@heymann.dev>
2024-07-10 11:17:26 +0200

    added lightdm

--- install_wm.sh
+++ install_wm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt-get install -y i3blocks i3status i3-wm compton flameshot
+sudo apt-get install -y i3blocks i3status i3-wm compton flameshot lightdm
+sudo systemctl enable lightdm.service
+
 
 mkdir -p .config
 cp -r .config/* ~/.config/
